d8dfd3de20ab4f2171644c999e8b86ed0bbd21bd
Vivek Kumar <35356580+kumarvivekpandey@users.noreply.github.com>
2023-03-21 10:50:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
 
 import './App.css';
-var LinkedStateMixin = require('react-addons-linked-state-mixin');
-var createReactClass = require('create-react-class');
 
-var WithoutLink = createReactClass({
-  mixins: [LinkedStateMixin],
-  getInitialState: function() {
-    return {message: 'Hello!'};
-  },
-  render: function() {
-    var valueLink = this.linkState('message');
-    var handleChange = function(e) {
-      valueLink.requestChange(e.target.value);
-    };
-    return <input type="text" value={valueLink.value} onChange={handleChange} />;
-  }
-});
 
-function App() {
+function Profile() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+    />
   );
 }
 
-export default App;
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
+
+
